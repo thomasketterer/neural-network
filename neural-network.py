18aa8ff1b2ef6246e4510b08cfc30dbeb2da4dc7
Thomas Ketterer <thomasketterer3@gmail.com>
2025-02-25 23:18:54 -0500

    Create neural-network.py

---
+++ neural-network.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+n = [2, 3, 3, 1]
+
+W1 = np.random.randn(n[1], n[0])
+W2 = np.random.randn(n[2], n[1])
+W3 = np.random.randn(n[3], n[2])
+b1 = np.random.randn(n[1], 1)
+b2 = np.random.randn(n[2], 1)
+b3 = np.random.randn(n[3], 1)
+
+X = np.array([
+    [150, 70],
+    [254, 73],
+    [312, 68],
+    [120, 60],
+    [154, 61],
+    [212, 65],
+    [216, 67],
+    [145, 67],
+    [184, 64],
+    [130, 69]
+])
+
+A0 = X.T
+print(A0.shape)
