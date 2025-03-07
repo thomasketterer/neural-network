4fd08c53ab9b5cab3639a24e2d75a4a0664cd4af
Thomas Ketterer <thomasketterer3@gmail.com>
2025-03-06 23:18:48 -0500

    Making functions

--- neural-network.py
+++ neural-network.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+L = 3
 n = [2, 3, 3, 1]
 
 W1 = np.random.randn(n[1], n[0])
@@ -22,21 +23,38 @@ X = np.array([
     [130, 69]
 ])
 
-A0 = X.T #transpose X
 
-y = np.array([
-    0,
-    1,
-    1,
-    0,
-    0,
-    1,
-    1,
-    0,
-    1,
-    0
-])
-m = 10
+def prepare_data() :
+    y = np.array([0,1,1,0,0,1,1,0,1,0])
+    m = 10
+    A0 = X.T
+    Y = y.reshape(n[L], m)
+    print(Y)
+    return A0, Y
 
-Y = y.reshape(n[3], m)
-Y.shape
+
+def sigmoid(arr):
+    return 1 / (1 + np.exp(-1 * arr))
+
+def feed_forward(A0):
+    Z1 = W1 @ A0 + b1
+    A1 = sigmoid(Z1)
+
+    Z2 = W2 @ A1 + b2
+    A2 = sigmoid(Z2)
+
+    Z3 = W3 @ A2 + b3
+    A3 = sigmoid(Z3)
+
+    y_hat = A3
+    return y_hat
+
+A0, Y = prepare_data()
+y_hat = feed_forward(A0)
+print(y_hat)
+
+
+
+
+
+
